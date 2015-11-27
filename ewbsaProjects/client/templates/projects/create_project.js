7b105a10008bd44748ff9697cc5f90ca5d8a4c3b
Kyle Meenehan <kylemeenehan@gmail.com>
2015-11-27 11:40:56 +0200

    Changed project submit to a method

--- ewbsaProjects/client/templates/projects/create_project.js
+++ ewbsaProjects/client/templates/projects/create_project.js
@@ -8,7 +8,12 @@ Template.createProject.events({
 			summary: $(e.target).find('#project-summary').text()
 		};
 
-		project._id = Projects.insert(project);
-		Router.go('projectPage', project);
+		Meteor.call('projectInsert', project, function(error, result) {
+			// display the error to the user and abort
+			if (error){
+				return alert(error.reason);
+			}
+			Router.go('projectPage', {_id: result._id});
+		});
 	}
 });
--- ewbsaProjects/lib/collections/projects.js
+++ ewbsaProjects/lib/collections/projects.js
@@ -5,3 +5,26 @@ Projects.allow({
 		return !! userId;
 	}
 });
+
+Meteor.methods({
+	projectInsert: function(postAttributes){
+		check(Meteor.userId(), String);
+		check(postAttributes,{
+			name: String,
+			blurb: String,
+			summary: String
+		})
+
+		var user = Meteor.user();
+		var project = _.extend(postAttributes, {
+			creatorId: user._id,
+			submitted: new Date()
+		});
+
+		var projectId = Projects.insert(project);
+
+		return {
+			_id: projectId
+		};
+	}
+});
